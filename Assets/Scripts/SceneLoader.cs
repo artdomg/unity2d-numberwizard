164c27620220f1e7e94423db4b1ca4a96878eedd
Arturo Dominguez <artdomg@gmail.com>
2020-12-29 16:14:02 -0700

    initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadCoreGameScene()
+    {
+        SceneManager.LoadScene("Core Game");
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void LoadWinScreen()
+    {
+        SceneManager.LoadScene("Win Screen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
